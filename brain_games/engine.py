63b72204a52bb93caf3f61a56ed74bfa71d892f3
Alexander <lisimenko.alexander@yandex.ru>
2022-11-25 11:49:12 +0300

    update engine.py

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -30,8 +30,8 @@ def main(rule, question, correct_answer):
             score += 1
             print('Correct!')
         else:
-            print(f'"{answer}" is wrong answer ;(. Correct answer was "{correct_answer}"')
-            print(f"Let's try again, {name}")
+            print(f'"{answer}" is wrong answer ;(. Correct answer was "{correct_answer}".')
+            print(f"Let's try again, {name}.")
             break
     if score == 3:
         print(f'Congratulations, {name}!')

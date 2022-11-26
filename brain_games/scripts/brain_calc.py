1450cb48ec204ccbfbf80ab215616f0c7e718e44
Alexander <lisimenko.alexander@yandex.ru>
2022-11-26 11:51:53 +0300

    update scripts/brain_calc.py

--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -2,9 +2,10 @@
 from brain_games import engine
 from brain_games.games import brain_calc
 
+
 def main():
     engine.main(brain_calc.rule, brain_calc.question_and_answer)
 
+
 if __name__ == '__main__':
     main()
-

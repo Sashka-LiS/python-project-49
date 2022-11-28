307cdb9e63de3375321da2eb0ce3ee0dec1ac69a
Alexander <lisimenko.alexander@yandex.ru>
2022-11-28 13:18:19 +0300

    add games/brain_gcd.py

---
+++ brain_games/games/brain_gcd.py
@@ -0,0 +1,16 @@
+import random
+import math
+
+
+rule = 'Find the greatest common divisor of given numbers.'
+
+
+def question_and_answer():
+    '''вернуть вопрос и правильный ответ'''
+
+    num1 = random.randint(1, 100)
+    num2 = random.randint(1, 100)
+
+    question = f'Question: {num1} {num2}'
+    correct_answer = math.gcd(num1, num2)
+    return question, correct_answer
